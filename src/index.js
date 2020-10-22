8f9ea68fc42a2e2c181266ddd6c0b4a522a7a4c8
Анастасия <nastenka_gubarenko@mail.ru>
2020-10-22 16:14:07 +0800

    Form_Find_Number

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 // JS
-import './js/'
-
+import "./js/"
+import "air-datepicker/dist/js/datepicker.min";
 // SCSS
 import './pages/main.scss'
+import "air-datepicker/dist/css/datepicker.min.css"
 
 
