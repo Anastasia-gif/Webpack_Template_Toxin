64aa0f9404b3362a5597cbe9ad3c87706a97877e
Анастасия <nastenka_gubarenko@mail.ru>
2020-10-23 18:52:44 +0800

    logo_Header

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 // JS
-import "./js/"
-import "air-datepicker/dist/js/datepicker.min";
+import './js/'
+
 // SCSS
 import './pages/main.scss'
-import "air-datepicker/dist/css/datepicker.min.css"
 
 
